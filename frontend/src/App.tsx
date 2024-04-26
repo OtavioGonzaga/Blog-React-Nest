5e4b452d1420830fa710ec878782bbb704cf93f0
OtavioGonzaga <otaviolgonzaga@gmail.com>
2024-04-26 15:47:11 -0300

    initial config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+	const [nest, setNest] = useState('wait...');
 
-export default App;
+	useEffect(() => {
+		axios
+			.get(`${process.env.REACT_APP_NEST_URL}`)
+			.then(res => {
+				setNest(res.data);
+			})
+			.catch(err => {
+				console.error(err);
+			});
+	}, []);
+
+	return (
+		<>
+			<div className="App">
+				<h1>{nest}</h1>
+			</div>
+		</>
+	);
+}
